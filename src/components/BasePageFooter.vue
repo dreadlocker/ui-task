8a1d8a0d6b1d1b67d192b2a10bd70c77a8909549
Svetozar <svetozariliev13@gmail.com>
2020-05-20 18:36:35 +0300

    Footer component done

--- src/components/BasePageFooter.vue
+++ src/components/BasePageFooter.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <div>
-      <span
+  <div class="footer-holder">
+    <div class="footer-links-holder mb-4 d-flex flex-column align-items-center">
+      <u
         v-for="link in footerInfo.links"
         :key="link"
-      >{{link}}</span>
+      >
+        <a
+          href=""
+          class="footer-link font-weight-light"
+        >{{link}}</a>
+      </u>
     </div>
 
-    <p>{{footerInfo.text}}</p>
+    <p class="footer-info pb-4 font-weight-light">{{footerInfo.text}}</p>
   </div>
 </template>
 
@@ -18,7 +23,7 @@ export default {
     return {
       footerInfo: {
         links: ["Privacy Policy", "Contact Us", "Terms and Conditions"],
-        text: "Disclaimer: Contract for differences traded on margin carry a high degree of risk. As such they may not be suitable for all investors. As it can result in loss of some or all invested capital, please seek independent advice and ensure you fully understand the risks associated with trading."
+        text: "Disclaimer: CFDs traded on margin carry a high degree of risk. As such they may not be suitable for all investors. As it can result in loss of some or all invested capital, please seek independent advice and ensure you fully understand the risks associated with trading."
       }
     }
   }
